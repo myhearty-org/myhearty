0393b8dbcda6b871e613c9f1bd79b1c63b7a907c
Ray Jasson <48130098+rayjasson98@users.noreply.github.com>
2022-04-22 05:32:04 +0800

    Add Tailwind CSS official class sorter (#7)
    * Add Tailwind CSS official class sorter
    
    * Add sorter plugin to standalone Prettier config
    
    * Remove Headwind sorter
    
    * Resort Tailwind CSS classes

--- components/layout/layout.tsx
+++ components/layout/layout.tsx
@@ -8,7 +8,7 @@ type LayoutProps = {
 
 export function Layout({ children }: LayoutProps) {
   return (
-    <div className="flex flex-col min-h-screen">
+    <div className="flex min-h-screen flex-col">
       <Header />
       <Main>{children}</Main>
       <Footer />
