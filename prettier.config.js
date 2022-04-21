854053370f20efd9b8a6cc06c6b4b8eed887c6cf
Ray Jasson <48130098+rayjasson98@users.noreply.github.com>
2022-04-22 05:22:55 +0800

    Add sorter plugin to standalone Prettier config

---
+++ prettier.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  bracketSameLine: true,
+  importOrderSeparation: true,
+  importOrderSortSpecifiers: true,
+  importOrderCaseInsensitive: true,
+  printWidth: 110,
+  singleQuote: true,
+  plugins: [require('prettier-plugin-tailwindcss')],
+};
