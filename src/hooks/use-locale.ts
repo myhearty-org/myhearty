f824894e07ec9024128cd3970f630ba159c3d60b
Ray Jasson <48130098+rayjasson98@users.noreply.github.com>
2022-04-24 02:15:12 +0800

    Change `const` to function

--- src/hooks/use-locale.ts
+++ src/hooks/use-locale.ts
@@ -1,6 +1,6 @@
 import { useTranslation } from 'next-i18next';
 
-export const useLocale = () => {
+export function useLocale() {
   const { i18n, t } = useTranslation('common');
 
   return { i18n, t };
