bdbdbe32815531428a607d63813025764abd8308
Ray Jasson <48130098+rayjasson98@users.noreply.github.com>
2022-04-24 01:27:38 +0800

    Add alert component

---
+++ src/components/ui/alert.tsx
@@ -0,0 +1,39 @@
+import { CheckCircleIcon, ExclamationIcon, InformationCircleIcon, XCircleIcon } from '@heroicons/react/solid';
+import cn from 'classnames';
+import { ReactNode } from 'react';
+
+export type AlertProps = {
+  title?: ReactNode;
+  message?: ReactNode;
+  actions?: ReactNode;
+  className?: string;
+  severity: 'success' | 'warning' | 'error' | 'info';
+};
+
+export function Alert({ title, message, actions, className, severity }: AlertProps) {
+  return (
+    <div
+      className={cn(
+        'rounded border border-opacity-20 p-3',
+        className,
+        severity === 'error' && 'border-red-900 bg-red-50 text-red-800',
+        severity === 'warning' && 'border-yellow-700 bg-yellow-50 text-yellow-700',
+        severity === 'info' && 'border-sky-700 bg-sky-50 text-sky-700',
+        severity === 'success' && 'bg-gray-900 text-white'
+      )}>
+      <div className="flex">
+        <div className="flex-shrink-0">
+          {severity === 'error' && <XCircleIcon className="h-5 w-5 text-red-40" aria-hidden="true" />}
+          {severity === 'warning' && <ExclamationIcon className="h-5 w-5 text-yellow-400" aria-hidden="true" />}
+          {severity === 'info' && <InformationCircleIcon className="h-5 w-5 text-sky-400" aria-hidden="true" />}
+          {severity === 'success' && <CheckCircleIcon className="h-5 w-5 text-gray-400" aria-hidden="true" />}
+        </div>
+        <div className="ml-3 flex-grow">
+          <h3 className="text-sm font-medium">{title}</h3>
+          <div className="text-sm">{message}</div>
+        </div>
+        {actions && <div className="text-sm">{actions}</div>}
+      </div>
+    </div>
+  );
+}
