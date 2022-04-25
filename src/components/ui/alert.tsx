3ece5d44ea047cd8dba4d00d20bb8520b01d634b
Ray Jasson <48130098+rayjasson98@users.noreply.github.com>
2022-04-25 19:46:30 +0800

    Modify appearance of alert component

--- src/components/ui/alert.tsx
+++ src/components/ui/alert.tsx
@@ -16,17 +16,17 @@ export function Alert({ title, message, actions, className, severity }: AlertPro
       className={cn(
         'rounded border border-opacity-20 p-3',
         className,
-        severity === 'error' && 'border-red-900 bg-red-50 text-red-800',
+        severity === 'error' && 'border-red-900 bg-red-50 text-red-600',
         severity === 'warning' && 'border-yellow-700 bg-yellow-50 text-yellow-700',
         severity === 'info' && 'border-sky-700 bg-sky-50 text-sky-700',
-        severity === 'success' && 'bg-gray-900 text-white'
+        severity === 'success' && 'border-green-900 bg-green-50 text-green-600'
       )}>
       <div className="flex">
         <div className="flex-shrink-0">
-          {severity === 'error' && <XCircleIcon className="h-5 w-5 text-red-40" aria-hidden="true" />}
+          {severity === 'error' && <XCircleIcon className="h-5 w-5 text-red-400" aria-hidden="true" />}
           {severity === 'warning' && <ExclamationIcon className="h-5 w-5 text-yellow-400" aria-hidden="true" />}
           {severity === 'info' && <InformationCircleIcon className="h-5 w-5 text-sky-400" aria-hidden="true" />}
-          {severity === 'success' && <CheckCircleIcon className="h-5 w-5 text-gray-400" aria-hidden="true" />}
+          {severity === 'success' && <CheckCircleIcon className="h-5 w-5 text-green-400" aria-hidden="true" />}
         </div>
         <div className="ml-3 flex-grow">
           <h3 className="text-sm font-medium">{title}</h3>
