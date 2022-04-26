b8378973ac01193ccce2c9b12cf33a170ac20608
Ray Jasson <48130098+rayjasson98@users.noreply.github.com>
2022-04-27 05:10:53 +0800

    Fix hydration mismatch problem (#14)
    * Add placeholder user page
    
    * Add `use-has-mounted` hook
    
    * Only render authenticated view on client-side

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,3 +1,4 @@
 export { useAuth } from './use-auth';
+export { useHasMounted } from './use-has-mounted';
 export { useLocalStorage } from './use-local-storage';
 export { useLocale } from './use-locale';
