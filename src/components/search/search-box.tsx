2c1f05ced4eaf44810011ff15bec0f3242d458bd
Ray Jasson <48130098+rayjasson98@users.noreply.github.com>
2022-05-01 05:39:55 +0800

    Add searchbox component

---
+++ src/components/search/search-box.tsx
@@ -0,0 +1,53 @@
+import { SearchIcon } from '@heroicons/react/solid';
+import { useBreakpoint, useHasMounted } from '@hooks/index';
+import { ChangeEvent, useRef, useState } from 'react';
+import { connectSearchBox } from 'react-instantsearch-dom';
+
+type CustomSearchBoxProps = {
+  currentRefinement: any;
+  refine: any;
+  placeholder: string;
+};
+
+function CustomSearchBox({ currentRefinement, refine, placeholder = '' }: CustomSearchBoxProps) {
+  const [value, setValue] = useState<any>(currentRefinement);
+  const debouncedRefineValueRef = useRef<any>();
+
+  function onChangeDebounced(event: ChangeEvent<HTMLInputElement>) {
+    const value = event.currentTarget.value;
+    clearTimeout(debouncedRefineValueRef.current);
+
+    debouncedRefineValueRef.current = setTimeout(() => {
+      refine(value);
+    }, 450);
+
+    setValue(value);
+  }
+
+  const hasMounted = useHasMounted();
+  const { sm } = useBreakpoint();
+
+  return (
+    <form
+      noValidate
+      action=""
+      role="search"
+      onSubmit={(e) => e.preventDefault()}
+      className="relative w-full px-0.5">
+      <div className="relative">
+        <div className="pointer-events-none absolute inset-y-0 left-3 flex items-center">
+          <SearchIcon className="h-6 w-6 text-gray-500" />
+        </div>
+        <input
+          type="search"
+          value={value}
+          onChange={onChangeDebounced}
+          placeholder={hasMounted ? (sm ? 'Search' : placeholder) : 'Search'}
+          className="w-full rounded-none border-transparent bg-white px-5 py-3 pl-10 text-xs text-black placeholder-gray-600 shadow-sm transition hover:bg-opacity-60 focus:border-pink-300 focus:outline-none focus:ring-2 focus:ring-pink-300 sm:text-sm"
+        />
+      </div>
+    </form>
+  );
+}
+
+export const SearchBox = connectSearchBox(CustomSearchBox);
