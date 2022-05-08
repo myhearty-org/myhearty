465806783a4c76f12b1d4f153a2138ced69abef9
Ray Jasson <48130098+rayjasson98@users.noreply.github.com>
2022-05-09 04:43:47 +0800

    Increase vertical paddings of `no-search-result` component

--- src/components/search/no-search-results.tsx
+++ src/components/search/no-search-results.tsx
@@ -11,7 +11,7 @@ function NoSearchResultsComponent({ searchQuery, nbHits }: NoSearchResultsCompon
     return null;
   } else {
     return (
-      <div className="flex w-full justify-center rounded border border-dashed border-gray-300 bg-white px-4">
+      <div className="flex w-full justify-center rounded border border-dashed border-gray-300 bg-white py-12 px-4">
         <div className="flex flex-col items-center self-center py-10 text-center sm:py-20">
           <div className="mt-6 mb-2 text-lg text-gray-700">
             Sorry, MyHearty doesn&apos;t have any resources on <b>&ldquo;{searchQuery}&rdquo;</b>
