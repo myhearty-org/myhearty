750ab960a806b1048d94e539f88fcc97d5701b13
Ray Jasson <48130098+rayjasson98@users.noreply.github.com>
2022-05-09 02:58:12 +0800

    Add charitable aids search page

--- src/pages/aids/index.tsx
+++ src/pages/aids/index.tsx
@@ -1,3 +1,57 @@
-export default function CharitableAidsPage() {
-  return <></>;
+import { Search } from '@components/charitable-aids';
+import { TYPESENSE_SERVER_CONFIG } from '@lib/config';
+import { NextSeo } from 'next-seo';
+import { GetServerSideProps } from 'next/types';
+import { useState } from 'react';
+import { findResultsState } from 'react-instantsearch-dom/server';
+import TypesenseInstantSearchAdapter from 'typesense-instantsearch-adapter';
+
+const typesense = new TypesenseInstantSearchAdapter({
+  server: TYPESENSE_SERVER_CONFIG,
+  geoLocationField: 'coordinates',
+  additionalSearchParameters: {
+    query_by: 'name,organization,location',
+    queryByWeights: '2,1,2',
+  },
+});
+
+const searchClient = typesense.searchClient;
+
+type CharitableAidsPageProps = {
+  resultsState: any;
+};
+
+export default function CharitableAidsPage({ resultsState }: CharitableAidsPageProps) {
+  const [searchState, setSearchState] = useState({});
+
+  function onSearchStateChange(searchState: any) {
+    setSearchState(searchState);
+  }
+
+  return (
+    <div className="flex-grow">
+      <NextSeo title="Search Charitable Aids" />
+      <Search
+        searchClient={searchClient}
+        searchState={searchState}
+        onSearchStateChange={onSearchStateChange}
+        resultsState={resultsState}
+      />
+    </div>
+  );
 }
+
+export const getServerSideProps: GetServerSideProps = async function ({ res }) {
+  res.setHeader('Cache-Control', `s-maxage=${1 * 60 * 60}, stale-while-revalidate=${24 * 60 * 60}`);
+
+  const resultsState = await findResultsState(Search, {
+    indexName: 'charitable_aids',
+    searchClient: searchClient,
+  });
+
+  return {
+    props: {
+      resultsState: JSON.parse(JSON.stringify(resultsState)),
+    },
+  };
+};
