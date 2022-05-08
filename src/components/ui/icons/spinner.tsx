65034b46c6f722622580bbfe928ff6fd115b1516
Ray Jasson <48130098+rayjasson98@users.noreply.github.com>
2022-05-09 03:02:33 +0800

    Clean up SVG props

--- src/components/ui/icons/spinner.tsx
+++ src/components/ui/icons/spinner.tsx
@@ -8,9 +8,8 @@ export function Spinner({ theme }: SpinnerProps) {
   return (
     <svg
       className={cn('mx-4 h-5 w-5 animate-spin', theme === 'light' ? 'text-white' : 'text-pink-400')}
-      xmlns="http://www.w3.org/2000/svg"
-      fill="none"
-      viewBox="0 0 24 24">
+      viewBox="0 0 24 24"
+      fill="none">
       <circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" strokeWidth="4"></circle>
       <path
         className="opacity-75"
