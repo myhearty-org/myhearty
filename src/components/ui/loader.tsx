83853432fa079dd78575900ff192d7e93bdd485f
Ray Jasson <48130098+rayjasson98@users.noreply.github.com>
2022-05-09 21:14:54 +0800

    Allow loader to occupy the whole screen

--- src/components/ui/loader.tsx
+++ src/components/ui/loader.tsx
@@ -4,7 +4,7 @@ type LoaderProps = {
 
 export function Loader({ text }: LoaderProps) {
   return (
-    <div className="flex flex-col items-center gap-5">
+    <div className="flex grow flex-col items-center justify-center gap-5">
       <span className="loader" />
       {text && <span className="text-sm font-medium">{text}</span>}
     </div>
