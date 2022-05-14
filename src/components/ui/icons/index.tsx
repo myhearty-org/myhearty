89dadda91ddc81935195fd31cf98ccd24bee140d
Ray Jasson <48130098+rayjasson98@users.noreply.github.com>
2022-05-14 19:40:53 +0800

    Add icons for categories

--- src/components/ui/icons/index.tsx
+++ src/components/ui/icons/index.tsx
@@ -1,4 +1,11 @@
 export { CalendarXIcon } from './calendar-x-icon';
 export { CrosshairIcon } from './crosshair-icon';
+export { ElderlyIcon } from './elderly-icon';
+export { EscalatorWarningIcon } from './escalator-warning-icon';
+export { FamilyRestroomIcon } from './family-restroom-icon';
+export { FemaleIcon } from './female-icon';
 export { Logo } from './logo';
+export { PawPrintIcon } from './paw-print-icon';
+export { PoolIcon } from './pool-icon';
 export { Spinner } from './spinner';
+export { WheelchairIcon } from './wheelchair-icon';
