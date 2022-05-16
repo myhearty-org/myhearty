f5d516ae45abf656258fe3b27ab75adbaa72848e
Ray Jasson <48130098+rayjasson98@users.noreply.github.com>
2022-05-16 17:06:13 +0800

    Extract `login-form` and `signup-form` into individual components

---
+++ src/components/auth/index.tsx
@@ -0,0 +1,2 @@
+export { LoginForm } from './login-form';
+export { SignupForm } from './signup-form';
---
+++ src/components/auth/login-form.tsx
@@ -0,0 +1,46 @@
+import { Alert } from '@components/ui/alert';
+import { Button } from '@components/ui/button';
+import { EmailField, Form, PasswordField } from '@components/ui/form';
+import { useAuth } from '@hooks/index';
+import { useState } from 'react';
+import { useForm } from 'react-hook-form';
+
+type LoginFormData = {
+  email: string;
+  password: string;
+};
+
+export function LoginForm() {
+  const form = useForm<LoginFormData>();
+  const { formState, register } = form;
+  const [errorMessage, setErrorMessage] = useState<string>();
+
+  const auth = useAuth();
+
+  async function logIn({ email, password }: LoginFormData) {
+    try {
+      setErrorMessage('');
+
+      await auth.logIn(email, password);
+    } catch (error) {
+      setErrorMessage(error.response.data.error);
+    }
+  }
+
+  return (
+    <Form className="mt-8 space-y-6" form={form} handleSubmit={logIn}>
+      <EmailField id="email" label="Email Address" required {...register('email')} />
+      <PasswordField
+        id="current-password"
+        label="Password"
+        autoComplete="current-password"
+        required
+        {...register('password')}
+      />
+      {errorMessage && <Alert severity="error" title={errorMessage} />}
+      <Button className="w-full justify-center" type="submit" loading={formState.isSubmitting}>
+        Log In
+      </Button>
+    </Form>
+  );
+}
