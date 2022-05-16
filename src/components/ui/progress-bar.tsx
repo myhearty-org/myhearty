968bce519200e74bc6447f5c823a6e1037babf23
Ray Jasson <48130098+rayjasson98@users.noreply.github.com>
2022-05-17 02:29:13 +0800

    Set maximum width of progress bar to 100% only

--- src/components/ui/progress-bar.tsx
+++ src/components/ui/progress-bar.tsx
@@ -7,6 +7,8 @@ type ProgressBarProps = {
 };
 
 export function ProgressBar({ className, color, percentage }: ProgressBarProps) {
+  percentage = Math.min(percentage, 100);
+
   return (
     <div className={cn('h-2.5 w-full rounded-full bg-gray-100', className)}>
       <div className={cn('h-2.5 rounded-full', color)} style={{ width: `${percentage}%` }} />
