bf7c6b487ced6ab020f312fe720bfe300593509e
Ray Jasson <48130098+rayjasson98@users.noreply.github.com>
2022-05-16 20:06:12 +0800

    Refactor `donation-form` into `donation-dialog-form`

--- src/components/fundraising-campaigns/layout/donation-button.tsx
+++ src/components/fundraising-campaigns/layout/donation-button.tsx
@@ -8,23 +8,30 @@ import { onlyPositiveInteger } from '@utils/common';
 import { useState } from 'react';
 import { useForm } from 'react-hook-form';
 
-type DonationFormProps = {
+type DonationDialogFormProps = {
+  open: boolean;
+  onOpenChange: (open: boolean) => void;
   fundraisingCampaignId: string;
   organization: Organization;
 };
 
-type DonationFormData = {
+type DonationDialogFormData = {
   defaultAmount: string;
   otherAmount: string;
 };
 
-function DonationForm({ fundraisingCampaignId, organization }: DonationFormProps) {
-  const form = useForm<DonationFormData>();
+function DonationDialogForm({
+  open,
+  onOpenChange,
+  fundraisingCampaignId,
+  organization,
+}: DonationDialogFormProps) {
+  const form = useForm<DonationDialogFormData>();
   const { formState, register, clearErrors } = form;
 
   const [showOtherAmountField, setShowOtherAmountField] = useState(false);
 
-  async function donate({ defaultAmount, otherAmount }: DonationFormData) {
+  async function donate({ defaultAmount, otherAmount }: DonationDialogFormData) {
     const defaultAmountValue = parseInt(defaultAmount) * 100;
     const otherAmountValue = parseInt(otherAmount) * 100;
     const donationAmount = isNaN(defaultAmountValue) ? otherAmountValue : defaultAmountValue;
@@ -49,58 +56,65 @@ function DonationForm({ fundraisingCampaignId, organization }: DonationFormProps
   }
 
   return (
-    <div className="flex flex-col gap-6">
-      <div className="flex flex-col gap-1">
-        <p className="break-words text-lg font-medium">Donation collected by {organization.name}</p>
-        <p className="text-sm">
-          MyHearty uses Stripe as the payment gateway. A 3% transaction fees will be charged for each
-          successful donation.
-        </p>
-      </div>
-      <Form className="mx-auto flex w-4/5 flex-col gap-6" form={form} handleSubmit={donate}>
-        <div className="grid grid-cols-2 justify-center gap-4">
-          <RadioButton
-            label="RM25"
-            value="25"
-            onClick={onClickDefaultAmount}
-            {...register('defaultAmount')}
-          />
-          <RadioButton
-            label="RM50"
-            value="50"
-            onClick={onClickDefaultAmount}
-            defaultChecked
-            {...register('defaultAmount')}
-          />
-          <RadioButton
-            label="RM100"
-            value="100"
-            onClick={onClickDefaultAmount}
-            {...register('defaultAmount')}
-          />
-          <RadioButton
-            label="Other amount"
-            value=""
-            onClick={onClickOtherAmount}
-            {...register('defaultAmount')}
-          />
+    <Dialog open={open} onOpenChange={onOpenChange}>
+      <DialogContent>
+        <div className="flex flex-col gap-6">
+          <div className="flex flex-col gap-1">
+            <p className="break-words text-lg font-medium">Donation collected by {organization.name}</p>
+            <p className="text-sm">
+              MyHearty uses Stripe as the payment gateway. A 3% transaction fees will be charged for each
+              successful donation.
+            </p>
+          </div>
+          <Form className="mx-auto flex w-4/5 flex-col gap-6" form={form} handleSubmit={donate}>
+            <div className="grid grid-cols-2 justify-center gap-4">
+              <RadioButton
+                label="RM25"
+                value="25"
+                onClick={onClickDefaultAmount}
+                {...register('defaultAmount')}
+              />
+              <RadioButton
+                label="RM50"
+                value="50"
+                onClick={onClickDefaultAmount}
+                defaultChecked
+                {...register('defaultAmount')}
+              />
+              <RadioButton
+                label="RM100"
+                value="100"
+                onClick={onClickDefaultAmount}
+                {...register('defaultAmount')}
+              />
+              <RadioButton
+                label="Other amount"
+                value=""
+                onClick={onClickOtherAmount}
+                {...register('defaultAmount')}
+              />
+            </div>
+            <NumericField
+              addOnLeading={<InputLeading>RM</InputLeading>}
+              visible={showOtherAmountField}
+              label="Other amount"
+              value=""
+              validate={onlyPositiveInteger}
+              required={showOtherAmountField}
+              {...register('otherAmount', { validate: { betweenAllowedAmounts } })}
+            />
+            <DialogFooter>
+              <Button type="submit" loading={formState.isSubmitting}>
+                Continue
+              </Button>
+            </DialogFooter>
+          </Form>
         </div>
-        <NumericField
-          addOnLeading={<InputLeading>RM</InputLeading>}
-          visible={showOtherAmountField}
-          label="Other amount"
-          value=""
-          validate={onlyPositiveInteger}
-          required={showOtherAmountField}
-          {...register('otherAmount', { validate: { betweenAllowedAmounts } })}
-        />
-        <DialogFooter>
-          <Button type="submit" loading={formState.isSubmitting}>
-            Continue
-          </Button>
-        </DialogFooter>
-      </Form>
-    </div>
+        <DialogClose asChild>
+          <XIcon className="absolute top-2.5 right-2.5 h-6 w-6 cursor-pointer text-gray-400 hover:text-gray-500" />
+        </DialogClose>
+      </DialogContent>
+    </Dialog>
   );
 }
 
@@ -112,19 +126,21 @@ type DonationButtonProps = {
 export function DonationButton({ fundraisingCampaignId, organization }: DonationButtonProps) {
   const [showDialog, setShowDialog] = useState(false);
 
+  function onDialogOpenChange(open: boolean) {
+    if (!open) setShowDialog(false);
+  }
+
   return (
     <>
       <Button className="w-full justify-center" onClick={() => setShowDialog(true)}>
         Donate Now
       </Button>
-      <Dialog open={showDialog} onOpenChange={(isOpen) => !isOpen && setShowDialog(false)}>
-        <DialogContent>
-          <DonationForm fundraisingCampaignId={fundraisingCampaignId} organization={organization} />
-          <DialogClose asChild>
-            <XIcon className="absolute top-2.5 right-2.5 h-6 w-6 cursor-pointer text-gray-400 hover:text-gray-500" />
-          </DialogClose>
-        </DialogContent>
-      </Dialog>
+      <DonationDialogForm
+        open={showDialog}
+        onOpenChange={onDialogOpenChange}
+        fundraisingCampaignId={fundraisingCampaignId}
+        organization={organization}
+      />
     </>
   );
 }
