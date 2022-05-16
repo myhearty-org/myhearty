0132e41e972564dee37d15576bc09aa678f24669
Ray Jasson <48130098+rayjasson98@users.noreply.github.com>
2022-05-16 15:19:52 +0800

    Remove redundant class name

--- src/components/layout/footer.tsx
+++ src/components/layout/footer.tsx
@@ -6,7 +6,7 @@ function LogoText() {
   return (
     <Link href="/">
       <a className="flex flex-col items-center gap-1 p-2">
-        <Logo className="" width={60} height={60} />
+        <Logo width={60} height={60} />
         <span className="inline-block whitespace-nowrap text-xl font-semibold">MyHearty</span>
       </a>
     </Link>
