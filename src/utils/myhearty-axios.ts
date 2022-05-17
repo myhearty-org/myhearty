9334c33db3a640a363304839220f9b4755de7810
Ray Jasson <48130098+rayjasson98@users.noreply.github.com>
2022-05-18 00:18:13 +0800

    Configure Axios interceptor (#26)
    * Remove `user` and `setuser` from `useAuth`
    
    - `storedUser` and `setStoreduser` already have the required states needed by the app
    
    * Configure axios with interceptor
    
    - If 401/403 response is returned, clear user storage and force reload the page to refresh the app state
    
    * Call donation function with interceptor

--- src/utils/myhearty-axios.ts
+++ src/utils/myhearty-axios.ts
@@ -1,4 +1,6 @@
+import { useAuth } from '@hooks/index';
 import axios from 'axios';
+import { useRouter } from 'next/router';
 
 axios.defaults.baseURL = process.env.NEXT_PUBLIC_API_URL;
 
@@ -18,4 +20,21 @@ axios.defaults.headers.put['Accept'] = 'application/json';
 axios.defaults.headers.patch['Accept'] = 'application/json';
 axios.defaults.headers.delete['Accept'] = 'application/json';
 
-export { axios };
+const defaultAxios = axios.create();
+const axiosWithAuth = axios.create();
+
+axiosWithAuth.interceptors.response.use(
+  function (response) {
+    return response;
+  },
+  function (error) {
+    if (error.response.status === 401 || error.response.status === 403) {
+      window.localStorage.removeItem('user')
+      window.location.href = '/login';
+    }
+
+    return Promise.reject(error);
+  }
+);
+
+export { defaultAxios as axios, axiosWithAuth };
