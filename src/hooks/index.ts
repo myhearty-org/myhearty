6c3a790bcaa1443dbea8892e9e1307b597a74e83
Ray Jasson <48130098+rayjasson98@users.noreply.github.com>
2022-05-25 05:01:14 +0800

    Remove `useLocale` hook

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -2,4 +2,3 @@ export { useAuth } from './use-auth';
 export { useBreakpoint } from './use-breakpoint';
 export { useHasMounted } from './use-has-mounted';
 export { useLocalStorage } from './use-local-storage';
-export { useLocale } from './use-locale';
--- src/hooks/use-locale.ts
+++
@@ -1,7 +0,0 @@
-import { useTranslation } from 'next-i18next';
-
-export function useLocale(namespace?: string) {
-  const { i18n, t } = useTranslation(namespace || 'common');
-
-  return { i18n, t };
-}
