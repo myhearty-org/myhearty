5726bad1420363232f3c23b3e582fe79fdac0f04
Ray Jasson <48130098+rayjasson98@users.noreply.github.com>
2022-05-26 00:23:40 +0800

    Add SEO component to login and signup pages

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,22 +1,26 @@
 import { LoginForm } from '@components/auth';
 import { NoAuth } from '@components/helpers';
+import { NextSeo } from 'next-seo';
 import Link from 'next/link';
 
 export default function LoginPage() {
   return (
-    <NoAuth>
-      <div className="flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
-        <div className="w-full max-w-sm space-y-6 sm:max-w-md">
-          <h2 className="mt-6 text-center text-2xl font-bold text-gray-900">
-            Welcome.
-            <br />
-            Log in to your account
-          </h2>
-          <LoginForm />
-          <SignupLink />
+    <>
+      <NextSeo title="Log in to MyHearty" />
+      <NoAuth>
+        <div className="flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
+          <div className="w-full max-w-sm space-y-6 sm:max-w-md">
+            <h2 className="mt-6 text-center text-2xl font-bold text-gray-900">
+              Welcome.
+              <br />
+              Log in to your account
+            </h2>
+            <LoginForm />
+            <SignupLink />
+          </div>
         </div>
-      </div>
-    </NoAuth>
+      </NoAuth>
+    </>
   );
 }
 
--- src/pages/signup.tsx
+++ src/pages/signup.tsx
@@ -1,22 +1,26 @@
 import { SignupForm } from '@components/auth';
 import { NoAuth } from '@components/helpers';
+import { NextSeo } from 'next-seo';
 import Link from 'next/link';
 
 export default function SignupPage() {
   return (
-    <NoAuth>
-      <div className="flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
-        <div className="w-full max-w-sm space-y-6 sm:max-w-md">
-          <h2 className="mt-6 text-center text-2xl font-bold text-gray-900">
-            Join us now.
-            <br />
-            Create a new account
-          </h2>
-          <SignupForm />
-          <LoginLink />
+    <>
+      <NextSeo title="Join MyHearty" />
+      <NoAuth>
+        <div className="flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
+          <div className="w-full max-w-sm space-y-6 sm:max-w-md">
+            <h2 className="mt-6 text-center text-2xl font-bold text-gray-900">
+              Join us now.
+              <br />
+              Create a new account
+            </h2>
+            <SignupForm />
+            <LoginLink />
+          </div>
         </div>
-      </div>
-    </NoAuth>
+      </NoAuth>
+    </>
   );
 }
 
