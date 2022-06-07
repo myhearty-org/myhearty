e6828f7745f1ef59d41e25574a134aed89163e78
Ray Jasson <48130098+rayjasson98@users.noreply.github.com>
2022-06-08 00:28:32 +0800

    Add initial files

---
+++ apps/dashboard/next.config.js
@@ -0,0 +1,15 @@
+const { i18n } = require('./next-i18next.config');
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  i18n,
+  reactStrictMode: false,
+  images: {
+    domains: ['loremflickr.com'],
+  },
+  experimental: {
+    externalDir: true,
+  },
+};
+
+module.exports = nextConfig;
---
+++ apps/dashboard/src/components/helpers/auth-required.tsx
@@ -0,0 +1,29 @@
+import { useAuth } from '@components/providers/auth-provider';
+import { useHasMounted } from '@myhearty/hooks';
+import { Loader } from '@myhearty/ui/loader';
+import { useRouter } from 'next/router';
+import { useEffect } from 'react';
+
+type AuthRequiredProps = {
+  children: React.ReactNode;
+};
+
+export function AuthRequired({ children }: AuthRequiredProps) {
+  const auth = useAuth();
+  const router = useRouter();
+
+  useEffect(() => {
+    if (!auth.isAuthenticated) {
+      router.push('/login');
+    }
+  }, [auth.isAuthenticated, router]);
+
+  const hasMounted = useHasMounted();
+  if (!hasMounted) return null;
+
+  if (!auth.isAuthenticated) {
+    return <Loader text="Redirecting to login page..." />;
+  }
+
+  return <>{children}</>;
+}
