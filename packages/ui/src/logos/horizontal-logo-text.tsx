3f9deba525eba2daab47dc98a6b3847bd02d57f3
Ray Jasson <48130098+rayjasson98@users.noreply.github.com>
2022-06-10 18:27:07 +0000

    Decrease font size of horizontal logo text

--- packages/ui/src/logos/horizontal-logo-text.tsx
+++ packages/ui/src/logos/horizontal-logo-text.tsx
@@ -4,7 +4,7 @@ export function HorizontalLogoText() {
   return (
     <div className="flex items-center p-2">
       <Logo className="mr-3" width={36} height={36} />
-      <span className="inline-block whitespace-nowrap text-xl font-semibold sm:text-lg">MyHearty</span>
+      <span className="inline-block whitespace-nowrap text-lg font-semibold">MyHearty</span>
     </div>
   );
 }
