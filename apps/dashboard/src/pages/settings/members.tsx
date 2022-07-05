24fa86677d81fd67dc0a4d1a6d76ba0dd6806e81
Ray Jasson <48130098+rayjasson98@users.noreply.github.com>
2022-07-05 10:43:54 +0000

    Show warning message on members page if the user is not an admin

--- apps/dashboard/src/pages/settings/members.tsx
+++ apps/dashboard/src/pages/settings/members.tsx
@@ -1,7 +1,19 @@
 import { SettingsLayout } from '@components/layouts';
+import { useAuth } from '@components/providers';
 import { Members } from '@components/settings/members';
+import { Alert } from '@myhearty/ui/alert';
 
 export default function MembersSettingPage() {
+  const auth = useAuth();
+
+  if (!auth.member.admin) {
+    return (
+      <div className="flex justify-center">
+        <Alert severity="warning" title="Only organization admin can manage members." />
+      </div>
+    );
+  }
+
   return <Members />;
 }
 
