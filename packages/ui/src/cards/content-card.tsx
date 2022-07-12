eda91cf0fb9026ff2fe37c03a1014033799e8ad2
Ray Jasson <48130098+rayjasson98@users.noreply.github.com>
2022-07-13 05:27:27 +0800

    Fix text

--- packages/ui/src/cards/content-card.tsx
+++ packages/ui/src/cards/content-card.tsx
@@ -7,7 +7,7 @@ type ContentCardProps = {
 export function ContentCard({ content }: ContentCardProps) {
   return (
     <div className="mx-[-1rem] flex w-[calc(100%+2rem)] flex-col gap-4 border-0 border-gray-200 bg-white py-3 px-3 shadow-md md:mx-0 md:w-full md:rounded-md md:border md:px-6">
-      <h2 className="border-b border-gray-200 pb-1 text-lg font-medium">About Campaign</h2>
+      <h2 className="border-b border-gray-200 pb-1 text-lg font-medium">About</h2>
       <SanitizedHTML className="prose max-w-none tracking-tight" html={content} />
     </div>
   );
